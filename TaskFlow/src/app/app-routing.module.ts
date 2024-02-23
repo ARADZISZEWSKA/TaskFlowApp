4bb88a3a06847a01bcdf06ed7ef77a562f5edaa6
amira <amira@DESKTOP-3UHDE2H>
2024-02-23 15:53:15 +0100

    Creating loader page

--- TaskFlow/src/app/app-routing.module.ts
+++ TaskFlow/src/app/app-routing.module.ts
@@ -4,12 +4,16 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
-    redirectTo: 'folder/Inbox',
+    redirectTo: 'loader',
     pathMatch: 'full'
   },
   {
-    path: 'folder/:id',
-    loadChildren: () => import('./folder/folder.module').then( m => m.FolderPageModule)
+    path: 'loader',
+    loadChildren: () => import('./pages/loader/loader.module').then( m => m.LoaderPageModule)
+  },
+  {
+    path: 'login',
+    loadChildren: () => import('./pages/login/login.module').then( m => m.LoginPageModule)
   }
 ];
 
