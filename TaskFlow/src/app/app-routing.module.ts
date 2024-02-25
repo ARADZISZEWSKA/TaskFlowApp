3c24b19fd1a050406ba6ddee6eace42e4a82806c
amira <amira@DESKTOP-3UHDE2H>
2024-02-25 01:55:19 +0100

    Creating HomePage

--- TaskFlow/src/app/app-routing.module.ts
+++ TaskFlow/src/app/app-routing.module.ts
@@ -18,6 +18,10 @@ const routes: Routes = [
   {
     path: 'register',
     loadChildren: () => import('./pages/register/register.module').then( m => m.RegisterPageModule)
+  },
+  {
+    path: 'home',
+    loadChildren: () => import('./pages/home/home.module').then( m => m.HomePageModule)
   }
 ];
 
