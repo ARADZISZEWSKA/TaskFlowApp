b8349db9020378bc22e8a8b872982aa31ff97c88
Agata <agata.ozorowska.123@gmail.com>
2024-04-22 18:42:32 +0200

    Aktualizacja loadera

--- src/app/pages/loader/loader.page.ts
+++ src/app/pages/loader/loader.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-loader',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoaderPage implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
+    // Automatyczne przekierowanie po 2 sekundach
+    setTimeout(() => {
+      this.router.navigateByUrl('/login');
+    }, 2000);
   }
 
 }
