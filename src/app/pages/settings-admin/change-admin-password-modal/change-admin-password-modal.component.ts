9dcf4093e0703d3cd089cb71359c231144303d90
Agata <agata.ozorowska.123@gmail.com>
2024-06-05 02:26:48 +0200

    Zmiana hasła admin- naprawione

--- src/app/pages/settings-admin/change-admin-password-modal/change-admin-password-modal.component.ts
+++ src/app/pages/settings-admin/change-admin-password-modal/change-admin-password-modal.component.ts
@@ -1,28 +1,39 @@
 import { Component } from '@angular/core';
 import { ModalController } from '@ionic/angular';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-change-admin-password-modal',
   templateUrl: './change-admin-password-modal.component.html',
   styleUrls: ['./change-admin-password-modal.component.scss'],
 })
-export class ChangeAdminPasswordModalComponent  {
+export class ChangeAdminPasswordModalComponent {
   currentPassword: string = ''; // Initialize to empty string
   newPassword: string = '';      // Initialize to empty string
   confirmNewPassword: string = ''; // Initialize to empty string
-  //nie wiem ??
 
-  constructor(private modalController: ModalController) {}
+  constructor(
+    private modalController: ModalController,
+    private userService: UserService // Corrected spelling here
+  ) {}
 
   dismissModal() {
     this.modalController.dismiss();
   }
 
-  changePassword() {
+  async changePassword() {
+    console.log(this.newPassword);
+    console.log(this.confirmNewPassword);
     if (this.newPassword === this.confirmNewPassword) {
-      // Add your password change logic here
-      console.log('Password changed successfully');
-      this.dismissModal();
+      this.userService.changePassword(this.currentPassword, this.newPassword, this.confirmNewPassword).subscribe(
+        response => {
+          console.log('Password changed successfully', response);
+          this.dismissModal();
+        },
+        error => {
+          console.error('Error changing password', error);
+        }
+      );
     } else {
       console.log('Passwords do not match');
     }
